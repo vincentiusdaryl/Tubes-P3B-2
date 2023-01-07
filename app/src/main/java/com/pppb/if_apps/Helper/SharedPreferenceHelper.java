4f20805bf59be9ba8b941cd5de40c70b2632a13d
Michael Alexander <Michaelalex2705@gmail.com>
2023-01-07 13:35:24 +0700

    Bikin class helper buat SharedPreference (belum tentu bener)

---
+++ app/src/main/java/com/pppb/if_apps/Helper/SharedPreferenceHelper.java
@@ -0,0 +1,60 @@
+package com.pppb.if_apps.Helper;
+
+import com.pppb.if_apps.Model.Key;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class SharedPreferenceHelper {
+
+    private static final String PREFERENCE_FILE_KEY = "PREFERENCE_FILE_KEY";
+
+    private SharedPreferences mSharedPreferences;
+    private Context mContext;
+    private SharedPreferences.Editor editor ;
+
+    public SharedPreferenceHelper(Context context) {
+        mContext = context;
+        mSharedPreferences = mContext.getSharedPreferences(PREFERENCE_FILE_KEY, Context.MODE_PRIVATE);
+        editor = mSharedPreferences.edit();
+    }
+
+        public void saveString(String key, String value) {
+            mSharedPreferences.edit().putString(key, value).apply();
+        }
+
+        public String getString(String key) {
+            return mSharedPreferences.getString(key, null);
+        }
+
+        public void saveInt(String key, int value) {
+            mSharedPreferences.edit().putInt(key, value).apply();
+        }
+
+        public int getInt(String key) {
+            return mSharedPreferences.getInt(key, 0);
+        }
+
+        public void saveBoolean(String key, boolean value) {
+            mSharedPreferences.edit().putBoolean(key, value).apply();
+        }
+
+        public boolean getBoolean(String key) {
+            return mSharedPreferences.getBoolean(key, false);
+        }
+
+        public void put(String key, String value){
+            editor.putString(key, value)
+                    .apply();
+
+        }
+        public void put(String key, Boolean value){
+            editor.putBoolean(key, value)
+                .apply();
+        }
+
+        public void clear(){
+            editor.clear()
+            .apply();
+        }
+
+}
